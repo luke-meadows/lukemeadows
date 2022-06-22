286c6dd48ade959e9b53ac51888170896699ecfe
luke-meadows <lukemeadowsdev@gmail.com>
2022-06-22 17:11:22 +0100

    lib(projectsData): add data for projects

--- src/lib/projectsData.js
+++ src/lib/projectsData.js
@@ -8,9 +8,9 @@ export const projectsData = {
     title: 'Connected Home Systems',
     text: [
       'This project was completed for a client in the smart home business. Connected Home Systems wanted to inject some energy back into their website with a completely overhauled design which appealed to their high end clientele.',
-      'Included in the build is a content management system. The administrator is able to create, edit and delete the content on the website. Blog posts, images and past projects can all be managed through the password protected portal.',
+      'Included in the build is a content management system. The administrator is able to create, edit and delete the content on their website. Blog posts, images and past projects can all be managed through the password protected portal.',
     ],
-    deliverables: ['UI Design', 'Development', 'CMS'],
+    deliverables: ['Design', 'Development', 'CMS'],
     urls: {
       code: 'https://github.com/luke-meadows/connected-home-systems',
       site: 'https://www.connectedhomesystems.co.uk',
@@ -24,10 +24,10 @@ export const projectsData = {
       'This project was completed for a client in the smart home business. Connected Home Systems wanted to inject some energy back into their website with a completely overhauled design which appealed to their high end clientele.',
       'Included in the build is a content management system. The administrator is able to create, edit and delete the content on the website. Blog posts, images and past projects can all be managed through the password protected portal.',
     ],
-    deliverables: ['UI Design', 'Development', 'CMS'],
+    deliverables: ['Design', 'Development', 'CMS'],
     urls: {
-      code: 'https://github.com/luke-meadows/connected-home-systems',
-      site: 'https://www.connectedhomesystems.co.uk',
+      code: 'https://github.com/luke-meadows/above-and-beyond-solutions',
+      site: 'https://www.aboveandbeyondsolutions.co.uk/',
     },
     logo: ABSLOGO,
     preview: ABSPREVIEW,
