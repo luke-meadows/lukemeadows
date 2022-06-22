46d84b76e456b2f7d877ac5aad4b3e60ed454951
luke-meadows <lukemeadowsdev@gmail.com>
2022-06-22 17:07:19 +0100

    feat(components/ContactForm): add contact for with functionality to send to email.js

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,11 +1,128 @@
+import { useRef, useState } from 'react';
 import styled from 'styled-components';
+import emailjs from '@emailjs/browser';
+
 export default function ContactForm() {
+  const [buttonText, setButtonText] = useState({
+    text: 'Send',
+    icon: 'paper-plane-empty',
+  });
+
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const messageRef = useRef();
+  const phoneRef = useRef();
+
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+    company: '',
+  });
+  const refs = {
+    name: nameRef,
+    email: emailRef,
+    phone: phoneRef,
+    message: messageRef,
+  };
+
+  function handleChange(e) {
+    buttonText.text === 'Sent' &&
+      setButtonText({
+        text: 'Send',
+        icon: 'paper-plane-empty',
+      });
+    const { value, name } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  }
+
+  function clearForm() {
+    setInputs({
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+      company: '',
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let shouldFormSubmit = true;
+    Object.keys(inputs).forEach((key) => {
+      if (key === 'company') return;
+      if (inputs[key].length < 1) {
+        refs[key].current.classList.add('nonono');
+        shouldFormSubmit = false;
+      }
+    });
+    if (!shouldFormSubmit) return;
+    emailjs
+      .send(
+        'service_3skfwsi',
+        'template_q403hfo',
+        inputs,
+        'user_tUEeIDH4Mdq7YlVipFLGr'
+      )
+      .then(
+        function (response) {
+          console.log('SUCCESS!', response.status, response.text);
+          clearForm();
+          setButtonText({ text: 'Sent', icon: 'ok' });
+          Object.keys(inputs).forEach((key) => {
+            refs[key].current.classList.remove('nonono');
+          });
+        },
+        function (error) {
+          console.log('FAILED...', error);
+        }
+      );
+  }
+
   return (
-    <Form>
-      <input type="text" placeholder="Name" name="name" />
-      <input type="email" placeholder="Email" name="email" />
-      <textarea placeholder="Message..." name="message" />
-      <button>Send</button>
+    <Form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={inputs.name}
+        onChange={handleChange}
+        ref={nameRef}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        name="email"
+        value={inputs.email}
+        onChange={handleChange}
+        ref={emailRef}
+      />
+      <input
+        type="telephone"
+        placeholder="Telephone"
+        name="phone"
+        value={inputs.phone}
+        onChange={handleChange}
+        ref={phoneRef}
+      />
+      <input
+        type="text"
+        placeholder="Company (optional)"
+        name="company"
+        value={inputs.company}
+        onChange={handleChange}
+      />
+      <textarea
+        placeholder="Message..."
+        name="message"
+        value={inputs.message}
+        onChange={handleChange}
+        ref={messageRef}
+      />
+      <button type="submit">
+        {buttonText.text} <i className={`icon-${buttonText.icon}`} />
+      </button>
     </Form>
   );
 }
@@ -20,7 +137,7 @@ const Form = styled.form`
   button {
     background: none;
     margin: 1rem 0;
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     font-family: 'Poppins';
     padding-bottom: 0.5rem;
     outline: none;
@@ -31,17 +148,35 @@ const Form = styled.form`
     height: fit-content;
   }
   textarea {
-    padding-bottom: 0;
+    padding-bottom: 0rem;
+    height: 6rem;
   }
   button {
-    border: 2px solid var(--almond);
-    padding: 0.5rem 6rem;
+    border: 2px solid var(--white);
+    padding: 0.5rem 3rem;
     border-radius: 5px;
-    background: var(--almond);
+    color: var(--white);
     width: fit-content;
-    &:hover {
+    transition: all 0.2s ease-in-out;
+    display: flex;
+    align-items: center;
+    &:hover,
+    &:focus {
       transform: scale(1.01);
       cursor: pointer;
+      border: 2px solid var(--almond);
+      color: var(--almond);
     }
+    i {
+      margin-left: 0.25rem;
+      margin-bottom: 0.1rem;
+      font-size: 1rem;
+    }
+    .icon-ok {
+      color: green;
+    }
+  }
+  .nonono {
+    border-bottom: 2px solid var(--almond);
   }
 `;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -2,6 +2,7 @@ import ContactForm from '../contact/ContactForm';
 import Heading from '../Heading';
 import Hero from '../Hero';
 import Container from '../main/Container';
+import Footer from '../main/Footer';
 import PageTransitionDiv from '../main/PageTransitionDiv';
 export default function Contact() {
   return (
@@ -10,8 +11,30 @@ export default function Contact() {
         <Heading color="black">contact.</Heading>
       </Hero>
       <Container bg="black" color="white">
+        <p
+          style={{
+            color: 'var(--white)',
+            maxWidth: '800px',
+            margin: ' 0 auto 2rem auto',
+            fontSize: '1.5rem',
+            fontFamily: 'SpecifyPERSONAL',
+            letterSpacing: '1px',
+            color: 'var(--white)',
+          }}
+        >
+          Have you got a project or job opportunity you'd like to discuss?
+          <span
+            style={{
+              color: 'var(--almond)',
+            }}
+          >
+            {' '}
+            Send me a message!
+          </span>
+        </p>
         <ContactForm />
       </Container>
+      <Footer />
     </PageTransitionDiv>
   );
 }
